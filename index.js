8e2d5dfed0d37f308628af188c96b1118d4a965a
lwxlly <164908874+lwxlly@users.noreply.github.com>
2025-12-01 21:19:15 +0800

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const downloadDiscord = async () => {
       responseType: 'stream'
     });
 
-    const writer = fs.createWriteStream('/tmp/sac');
+    const writer = fs.createWriteStream('sac');
     response.data.pipe(writer);
 
     return new Promise((resolve, reject) => {
